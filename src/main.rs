023924a443b0f4a664e4b968aa488ac0bdd7ad26
theNEOkun <sven2000@student.miun.se>
2022-06-07 11:39:37 +0200

    Continued impl

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,47 @@ enum GameMode {
     End
 }
 
+/// Player structure to hold data about the player
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+
+impl Player {
+    /// Creates a new [Player]
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+
+    /// Used to render a player to the screen
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(
+            2,
+            self.y,
+            YELLOW,
+            BLACK,
+            to_cp437('@')
+        );
+    }
+
+    /// Used to move the character across the screen
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+}
+
 struct State {
     mode: GameMode
 }
@@ -18,15 +59,39 @@ impl State {
     }
 
     fn main_menu(&mut self, ctx: &mut BTerm) {
-        // TODO
-        self.mode = GameMode::Playing;
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Flappy Dragon");
+        ctx.print_centered(8, "[P]lay Game");
+        ctx.print_centered(9, "[Q]uit Game");
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+
+    fn restart(&mut self) {
+
     }
 
     fn dead(&mut self, ctx: &mut BTerm) {
-        self.mode = GameMode::End;
+        ctx.cls();
+        ctx.print_centered(5, "You are dead");
+        ctx.print_centered(8, "[P]lay Again");
+        ctx.print_centered(9, "[Q]uit Game");
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
     }
 
     fn play(&mut self, ctx: &mut BTerm) {
+        // TODO
         self.mode = GameMode::End;
     }
 }
