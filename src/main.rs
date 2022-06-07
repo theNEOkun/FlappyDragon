9c554479f6d766d9591d79dc892266678a6c3303
theNEOkun <sven2000@student.miun.se>
2022-06-07 12:11:08 +0200

    Book-complete

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use bracket_lib::prelude::*;
 enum GameMode {
     Menu,
     Playing,
-    End
+    End,
 }
 
 /// Player structure to hold data about the player
@@ -25,13 +25,7 @@ impl Player {
 
     /// Used to render a player to the screen
     fn render(&mut self, ctx: &mut BTerm) {
-        ctx.set(
-            0,
-            self.y,
-            YELLOW,
-            BLACK,
-            to_cp437('@')
-        );
+        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'));
     }
 
     /// Used to move the character across the screen
@@ -51,10 +45,50 @@ impl Player {
     }
 }
 
+struct Obstacle {
+    x: i32,
+    gap_y: i32,
+    size: i32,
+}
+
+impl Obstacle {
+    fn new(x: i32, score: i32) -> Self {
+        let mut random = RandomNumberGenerator::new();
+        Obstacle {
+            x,
+            gap_y: random.range(10, 40),
+            size: i32::max(2, 20 - score),
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm, player_x: i32) {
+        let screen_x = self.x - player_x;
+        let half_size = self.size / 2;
+
+        for y in 0..self.gap_y - half_size {
+            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'));
+        }
+
+        for y in self.gap_y + half_size..SCREEN_HEIGHT {
+            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'));
+        }
+    }
+
+    fn hit_obstacle(&self, player: &Player) -> bool {
+        let half_size = self.size / 2;
+        let does_x_match = player.x == self.x;
+        let player_above_gap = player.y < self.gap_y - half_size;
+        let player_below_gap = player.y > self.gap_y + half_size;
+        does_x_match && (player_above_gap || player_below_gap)
+    }
+}
+
 struct State {
     player: Player,
+    obstacle: Obstacle,
     frame_time: f32,
-    mode: GameMode
+    mode: GameMode,
+    score: i32,
 }
 
 const SCREEN_WIDTH: i32 = 80;
@@ -65,8 +99,10 @@ impl State {
     fn new() -> Self {
         State {
             player: Player::new(5, 25),
+            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
             frame_time: 0.0,
-            mode: GameMode::Menu
+            mode: GameMode::Menu,
+            score: 0,
         }
     }
 
@@ -86,13 +122,16 @@ impl State {
 
     fn restart(&mut self) {
         self.player = Player::new(5, 25);
+        self.obstacle = Obstacle::new(SCREEN_WIDTH, 0);
         self.frame_time = 0.0;
         self.mode = GameMode::Playing;
+        self.score = 0;
     }
 
     fn dead(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print_centered(5, "You are dead");
+        ctx.print_centered(6, &format!("Previous Score: {}", self.score));
         ctx.print_centered(8, "[P]lay Again");
         ctx.print_centered(9, "[Q]uit Game");
         if let Some(key) = ctx.key {
@@ -115,8 +154,18 @@ impl State {
             self.player.flap();
         }
         self.player.render(ctx);
+
         ctx.print(0, 0, "Press SPACE to flap");
-        if self.player.y > SCREEN_HEIGHT {
+        ctx.print(0, 1, &format!("Current Score: {}", self.score));
+
+        self.obstacle.render(ctx, self.player.x);
+
+        if self.player.x > self.obstacle.x {
+            self.score += 1;
+            self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score);
+        }
+
+        if self.player.y > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player) {
             self.mode = GameMode::End;
         }
     }
@@ -133,6 +182,8 @@ impl GameState for State {
 }
 
 fn main() -> BError {
-    let context = BTermBuilder::simple80x50().with_title("Flappy Dragon").build()?;
+    let context = BTermBuilder::simple80x50()
+        .with_title("Flappy Dragon")
+        .build()?;
     main_loop(context, State::new())
 }
