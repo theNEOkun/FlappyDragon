e56dd1eb5dfd0e8533983f896509482963a4aeaa
theNEOkun <sven2000@student.miun.se>
2022-06-07 11:09:19 +0200

    Working on implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use bracket_lib::prelude::*;
+
+enum GameMode {
+    Menu,
+    Playing,
+    End
+}
+
+struct State {
+    mode: GameMode
+}
+
+impl State {
+    fn new() -> Self {
+        State {
+            mode: GameMode::Menu
+        }
+    }
+
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        // TODO
+        self.mode = GameMode::Playing;
+    }
+
+    fn dead(&mut self, ctx: &mut BTerm) {
+        self.mode = GameMode::End;
+    }
+
+    fn play(&mut self, ctx: &mut BTerm) {
+        self.mode = GameMode::End;
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::End => self.dead(ctx),
+            GameMode::Playing => self.play(ctx),
+        }
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50().with_title("Flappy Dragon").build()?;
+    main_loop(context, State::new())
 }
